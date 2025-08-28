a3244a8859d87e19d275171387c063dbe3d02699
hectorlag <hectorvlaguna@gmail.com>
2025-08-28 19:15:06 -0300

    feat: configurar BD y crear BaseEntity con auditoría automática

--- src/main/java/com/centromedico/sistema_turnos/SistemaTurnosApplication.java
+++ src/main/java/com/centromedico/sistema_turnos/SistemaTurnosApplication.java
@@ -2,8 +2,10 @@ package com.centromedico.sistema_turnos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SistemaTurnosApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/centromedico/sistema_turnos/model/BaseEntity.java
@@ -0,0 +1,51 @@
+package com.centromedico.sistema_turnos.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "activo")
+    private Boolean activo = true;
+
+    // Métodos de utilidad
+    public void desactivar() {
+        this.activo = false;
+    }
+
+    public void activar() {
+        this.activo = true;
+    }
+
+    public boolean isActivo() {
+        return activo != null && activo;
+    }
+}
